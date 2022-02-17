a330bfa15f8d5e68fc795026fa07c1a6fb102ed5
amunsee <97048406+amunsee@users.noreply.github.com>
2022-02-17 06:27:54 -0800

    Update assignment6.cpp

--- assignment6/assignment6.cpp
+++ assignment6/assignment6.cpp
@@ -37,25 +37,90 @@ class tree{
         }
 
         else if(current->value >= new_value){ //go left
-                    
+            prev = current;
             add_value(new_value, current->left, prev);
             
         }
 
         
         else{ //node value > new_value go right
+            prev = current;
             add_value(new_value, current->right, prev);
         }
 
     }
 
+    Node * find_successor(Node *current){
+        if(current == nullptr){
+            return current;
+        }
 
-    void remove_value(){
-        return;
+        else{//keep going until biggest value which is furthest right off first left
+            while(current->right != nullptr){
+                current = current->right;
+            }
+
+            return current;
+        }
+    }
+
+    Node * remove_value(int find, Node *current){
+        if(current == nullptr){
+            cout << find << " is not in the list" << endl;
+            return current; //nullptr remains nullptr
+        }
+
+        else if(current->value < find){
+            current->right = remove_value(find, current->right); //right either stays same or is changed after funct call
+        }
+
+        else if(current->value > find){
+            current->left = remove_value(find, current->left); //left either stays same or is changed after funct call
+        }
+
+        else{//if the current node  value is == to the value to remove
+            if(current->left == nullptr){//if right child or no children
+                if(current->right == nullptr){ //if no children
+                    current = nullptr;
+                }
+
+                else{//if right only child move it up one level
+                    Node *temp = current;
+                    current = current->right;
+                    delete temp; //clean up memory
+                }
+            
+               
+            }
+
+            else if(current->right == nullptr){ //only child on the left just move it up one level
+                Node *temp = current;
+                current = current->left;
+                delete temp; //clean up memory
+            }
+
+            //if two children find the successor
+            else{
+                Node *temp = find_successor(current->left);
+                root->value = temp->value;
+                current->left = remove_value(temp->value, current->left); //find biggest value that is smaller than the value to delete so go left once then as far right as possible.
+        
+            }
+        }
+
+        return current;
+
     }
 
     void traverse_tree(Node *node){
-        return;
+        if(node == nullptr){
+            return;
+        }
+
+        traverse_tree(node->left);
+        cout << node->value << endl;
+        traverse_tree(node->right);
+
     }
 
 
