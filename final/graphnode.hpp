d4eb5fce8a29db7deb29f33d1bb63e66dcd65af7
amunsee <97048406+amunsee@users.noreply.github.com>
2022-03-14 04:49:18 -0700

    Update graphnode.hpp

--- final/graphnode.hpp
+++ final/graphnode.hpp
@@ -1,11 +1,14 @@
 #include <string>
 #include <vector>
+#include <utility>
 
 using std::string;
 using std::vector;
+using std::pair;
 
 struct graphnode{
     string name;
-    vector <graphnode *> neighbors;
-
+    //vector <graphnode *> neighbors; //each index of the 2 vectors matches together, neighbors[0] weight between it and the start node is neighbor_weights[0]
+    //vector <int> neighbor_weights;
+    vector <pair <graphnode *, int>> neighbors; //vector holds pair  that contains the neighbor and weight of getting to neighbor
 };
