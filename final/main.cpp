e44963b4addc6e9c64fa06cb2c0d4aff23bb5fed
amunsee <97048406+amunsee@users.noreply.github.com>
2022-03-14 04:48:41 -0700

    Update main.cpp

--- final/main.cpp
+++ final/main.cpp
@@ -11,5 +11,28 @@ using std::endl;
 
 int main(){
 
+    graph *graphy = new graph();
+
+    graphy->add_node("blue");
+    graphy->add_node("green");
+    graphy->add_node("red");
+    graphy->add_node("yellow");
+
+
+    graphy->add_edge("red", "blue", 5);
+    graphy->add_edge("green", "blue", 2);
+    graphy->add_edge("red", "green", 6);
+    graphy->add_edge("red", "yellow", 3);
+
+    graphy->print_graph();
+
+    cout << "here" << endl;
+    //above line working
+    graphy->shortest_path("yellow", "blue");
+
+    cout <<"wooo made it to the end no issues" << endl;
+
+    
+
     return 0;
 }
