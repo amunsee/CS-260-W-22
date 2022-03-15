5e96f2184a6f8db9035d5396ef071a688be7e045
amunsee <97048406+amunsee@users.noreply.github.com>
2022-03-15 04:31:15 -0700

    Update main.cpp

--- final/main.cpp
+++ final/main.cpp
@@ -17,21 +17,27 @@ int main(){
     graphy->add_node("green");
     graphy->add_node("red");
     graphy->add_node("yellow");
+    graphy->add_node("pink");
 
 
     graphy->add_edge("red", "blue", 5);
     graphy->add_edge("green", "blue", 2);
     graphy->add_edge("red", "green", 6);
-    graphy->add_edge("red", "yellow", 3);
+    graphy->add_edge("red", "yellow", 3); //mst current works for my 4 nodes graph...but adding pink it goes back to adding repeats...
+    graphy->add_edge("yellow", "pink", 7);
+
 
     graphy->print_graph();
 
-    cout << "here" << endl;
+    //cout << "here" << endl;
     //above line working
     graphy->shortest_path("yellow", "blue");
 
-    cout <<"wooo made it to the end no issues" << endl;
+    //cout <<"wooo made it to the end no issues" << endl;
 
+    
+    cout << "mst time" << endl;
+    graphy->min_spanning_tree();
     
 
     return 0;
