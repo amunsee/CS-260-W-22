8186eb47a973976ca36660bdbabd7409ef74ec61
amunsee <97048406+amunsee@users.noreply.github.com>
2022-03-15 17:11:19 -0700

    Update main.cpp

--- final/main.cpp
+++ final/main.cpp
@@ -9,34 +9,38 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int main(){
+int main(){//driver file/tests to show graph and functions are working
 
-    graph *graphy = new graph();
+    graph *graphy = new graph(); 
 
-    graphy->add_node("blue");
+    graphy->add_node("blue"); 
     graphy->add_node("green");
     graphy->add_node("red");
     graphy->add_node("yellow");
     graphy->add_node("pink");
 
+    graphy->print_graph(); //should print each node with no neighbors indicated by just null after node name
+    cout << endl;
+
 
     graphy->add_edge("red", "blue", 5);
     graphy->add_edge("green", "blue", 2);
     graphy->add_edge("red", "green", 6);
-    graphy->add_edge("red", "yellow", 3); //mst current works for my 4 nodes graph...but adding pink it goes back to adding repeats...
+    graphy->add_edge("red", "yellow", 3); 
     graphy->add_edge("yellow", "pink", 7);
 
 
-    graphy->print_graph();
+    graphy->print_graph(); //verify that blue-pink nodes are  added to graph, and that the edges added above are added to the respective nodes
+    //prints out the node and all of its neighbors, the arrows dont indicate a list chain blue->red->green = blues neighbors are red and green.
+    //each set of neighbors is ended with null to indicate end of neighbor list
+    //should printout blue->red->green, green->blue->red, red->blue->green->yellow, yellow->red->pink, pink->yellow
 
-    //cout << "here" << endl;
-    //above line working
-    graphy->shortest_path("yellow", "blue");
+    cout << endl;
+    graphy->shortest_path("yellow", "blue"); //verify that shorest path from yellow to blue is yellow->red->blue nodes with a weight of 8
 
-    //cout <<"wooo made it to the end no issues" << endl;
 
-    
-    cout << "mst time" << endl;
+    cout << endl;
+    cout << "mst time" << endl; //should output blue->green, red->yellow, blue->red, pink->yellow with weight of 17
     graphy->min_spanning_tree();
     
 
